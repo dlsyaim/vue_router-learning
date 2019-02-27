5c6b9780cec261654d9cc5aa41302f5cb320633c
dlsyaim <dlsyaim@gmail.com>
2019-02-27 21:46:04 +0800

    页面跳转

---
+++ vue-router_lesson1/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '../components/HelloWorld'
+import About from '../components/About'
+import News from '../components/News'
+
+Vue.use(Router);
+
+export default new Router({
+    routes:[
+        {
+            path:'/',
+            name:'HelloWorld',
+            component:HelloWorld
+        },
+        {
+            path:'/about',
+            name:'About',
+            component:About
+        },
+        {
+            path:'/news',
+            name:'News',
+            component:News
+        }
+    ]
+})
